bd71c3c7328194ded0e526a681c0d93f075af894
endalosa <endalosa@gmail.com>
2017-08-02 22:32:16 -0500

    created shoe class

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,27 +3,26 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
-
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
+url(r'^admin/', admin.site.urls),
+url(r'^login/$', auth_views.login, name='login'),
+url(r'^logout/$', auth_views.logout, name='logout'),
 
-
-     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', auth_views.login, name='login'),
-
-
-    url(r'^logout/$', auth_views.logout, name='logout'),
-    url('^register/', CreateView.as_view(
+url('^register/', CreateView.as_view(
             template_name='register.html',
             form_class=UserCreationForm,
-            success_url='/profile'
-    )),
-
-            url('^profile/', CreateView.as_view(
+            success_url='/profile')),
+url('^profile/', CreateView.as_view(
                     template_name='profile.html',
-                    form_class=UserCreationForm,
-            )),
-
+                    form_class=UserCreationForm,)),
+url('^home/', CreateView.as_view(
+                        template_name='home.html',
+                        form_class=UserCreationForm,)),
+url('^closet/', CreateView.as_view(
+            template_name='closet.html',
+            form_class=UserCreationForm,)),
+            
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 def signup(request):
     if request.method == 'POST':
         uf = UserForm(request.POST, prefix='user')
