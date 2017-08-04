b263f9fdfc1094d389156bf83400cc1d4fc4e677
Iman <endalosa@gmail.com>
2017-08-04 10:12:05 -0500

    ?

--- happyfeet/views.py
+++ happyfeet/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from .models import MyShoe
-
+from .forms import ShoeForm
 # Create your views here.
 def get_closet(request):
     return render(request, 'closet.html',
     {'shoes': MyShoe.objects.filter(user=request.user)
     })
 
-def add_miles(request):
-    return render(request, 'addshoe.html',
-    {'shoes': MyShoe.objects.all()
-    })
+def create_shoe(request):
+    if request.method == "POST":
+        form = ShoeForm(request.POST)
+        if form.is_valid():
+            Shoe = form.save(commit=False)
+            Shoe.user = request.user
+            Shoe.name = form.save()
+            Shoe.brand = form.save()
+            Shoe.icon = form.save()
+            Shoe.miles_run = form.save()
+            Shoe.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = ShoeForm()
+        return render(request, 'addshoe.html',
+        {'shoes': MyShoe.objects.filter(user=request.user),
+        'form': form
+        })
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,11 @@ url('^register/', CreateView.as_view(
             template_name='register.html',
             form_class=UserCreationForm,
             success_url='/profile')),
+
+url('^addmiles/', CreateView.as_view(
+            template_name='addmiles.html',
+            form_class=UserCreationForm,)),            
+
 url('^profile/', CreateView.as_view(
                     template_name='profile.html',
                     form_class=UserCreationForm,)),
@@ -24,6 +29,8 @@ url('^home/', CreateView.as_view(
                         template_name='home.html',
                         form_class=UserCreationForm,)),
 url('^closet/', views.get_closet, name='closet'),
-url('^addshoe/', views.add_miles, name='addshoe'),
+url('^shoe/new/$', views.create_shoe, name='addshoe'),
+
+
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
